634252eb59d8addbe74019244269f719a57991d0
David Benko <dbenko@anypresence.com>
2014-06-16 20:18:56 -0400

    Include header

---
+++ DBTransitEncryption/DBTransitEncryption.h
@@ -0,0 +1,17 @@
+//
+//  DBTransitEncryption.h
+//  DBTransitEncryption
+//
+//  Created by David Benko on 6/16/14.
+//  Copyright (c) 2014 David Benko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef _DBTRANSITENCRYPTION_
+#define _DBTRANSITENCRYPTION_
+
+#import "DBTransitEncryptor.h"
+#import "DBTransitEncryptor+NSString.h"
+
+#endif /* _DBTRANSITENCRYPTION_ */
